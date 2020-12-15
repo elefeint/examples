80e0d7c824fa989c331c361205b883e697d7483c
Elena Felder <41136058+elefeint@users.noreply.github.com>
2020-12-15 15:02:04 -0500

    clean up imports

--- pubsub-rotate-credentials/src/main/java/com/example/CredentialRefreshConfig.java
+++ pubsub-rotate-credentials/src/main/java/com/example/CredentialRefreshConfig.java
@@ -5,23 +5,14 @@ import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
-import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
-import org.springframework.cloud.gcp.pubsub.integration.AckMode;
 import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
-import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
-import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.PublishSubscribeChannel;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.handler.annotation.Header;
 
 @Configuration
 public class CredentialRefreshConfig {
