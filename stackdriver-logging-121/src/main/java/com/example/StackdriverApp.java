7ed0148946b57506678a4406ece37b44364a06a4
Elena Felder <41136058+elefeint@users.noreply.github.com>
2021-07-02 13:15:35 -0400

    example of cloud logging (stackdriver) sample at a particular version

---
+++ stackdriver-logging-121/src/main/java/com/example/StackdriverApp.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackdriverApp {
+
+  private static final Log LOGGER = LogFactory.getLog(StackdriverApp.class);
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(StackdriverApp.class, args);
+
+    LOGGER.error("Blah blahety blah");
+
+    System.in.read();
+  }
+}
