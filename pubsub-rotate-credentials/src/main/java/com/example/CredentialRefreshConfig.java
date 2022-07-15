ec98376e10b23967918bad70c62eac43050deb30
Min Zhu <zhumin@google.com>
2022-07-15 19:19:29 +0000

    update dependency versions.

--- pubsub-rotate-credentials/src/main/java/com/example/CredentialRefreshConfig.java
+++ pubsub-rotate-credentials/src/main/java/com/example/CredentialRefreshConfig.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
-import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
+import com.google.cloud.spring.pubsub.integration.inbound.PubSubInboundChannelAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
